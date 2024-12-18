ae955f12e8fdaacf43335d03c5868fd5d4f05ac2
Salim <lordslaim22@gmail.com>
2024-12-18 14:42:55 +0100

    GetUser JWT in comments

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Post, Body, Delete, Param, Get, Put } from '@nestjs/common';
+import { Controller, Post, Body, Delete, Param, Get, Put, ParseIntPipe } from '@nestjs/common';
 import { CommentService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { JwtUser } from 'src/auth/interfaces/jwt-user.interface';
 
-@Controller('comments')
+@Controller('courses/posts')
 export class CommentController {
     constructor(private readonly commentService: CommentService) { }
 
     // Add a comment to a post by a user
-    @Post()
-    async addComment(@Body() createCommentDto: CreateCommentDto) {
-        const userId = "107fcda6-ebfc-4135-a1dd-e1003e608619";
-        const postId = 7
-        return this.commentService.createComment(createCommentDto, userId, postId);
+    @Post(":postId/comments")
+    async addComment(@Param("postId", ParseIntPipe) postId: number, @GetUser() user: JwtUser, @Body() createCommentDto: CreateCommentDto) {
+        return this.commentService.createComment(createCommentDto, user.id, postId);
     }
 
     // Get all comments for a specific post
